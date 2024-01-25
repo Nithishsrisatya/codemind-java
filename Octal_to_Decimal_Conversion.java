25bad458bac5690a9343dc1944db64e5c255155c
Nithish Sri Satya Korlapati <119840936+Nithishsrisatya@users.noreply.github.com>
2024-01-25 21:10:32 +0530

    import java.util.*; public class Main{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String n=sc.next();         int num = Integer.parseInt(n, 8);         System.out.print(num);     } }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String n=sc.next();
+        int num = Integer.parseInt(n, 8);
+        System.out.print(num);
+    }
+}

61e3bf62bedf5bd055ba9dc8ae2bc984770f33d6
Nithish Sri Satya Korlapati <119840936+Nithishsrisatya@users.noreply.github.com>
2024-01-25 20:49:42 +0530

    import java.util.*; public class Main{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         float k=sc.nextFloat();         float m;         m=(k*5)/18;         System.out.printf("%.2f",m);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        float k=sc.nextFloat();
+        float m;
+        m=(k*5)/18;
+        System.out.printf("%.2f",m);
+    }
+}

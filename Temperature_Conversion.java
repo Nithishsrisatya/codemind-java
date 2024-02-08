81b41ed2707323d2ed3eea18e4db5668b197e390
Nithish Sri Satya Korlapati <119840936+Nithishsrisatya@users.noreply.github.com>
2024-02-08 20:55:18 +0530

    import java.util.*; public class ASA{     public static void main(String[] args)     {         Scanner SC=new Scanner(System.in);         double n=SC.nextInt();         double F;         F=32+(n*9/5);         System.out.printf("%.2f",F);
        }
    }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ASA{
+    public static void main(String[] args)
+    {
+        Scanner SC=new Scanner(System.in);
+        double n=SC.nextInt();
+        double F;
+        F=32+(n*9/5);
+        System.out.printf("%.2f",F);
+        
+    }
+}
